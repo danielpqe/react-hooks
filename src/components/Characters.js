835de2f65fb9529550ecf2fc00ec6b3d64aea45b
danielpqe <danielpqe@gmail.com>
2022-03-07 23:07:12 -0500

    adding characters

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -2,15 +2,26 @@ import React, { useState, useEffect } from "react";
 
 export const Characters = () => {
   const [characters, setCharacters] = useState([]);
+  const [change, setChange] = useState(false);
 
   useEffect(() => {
-    fetch("https://rickandmortyapi.com/api/character/")
-      .then((response) => response.json())
-      .then((data) => setCharacters(data.results));
+    setCharacters([]);
+    setTimeout(() => {
+      fetch("https://rickandmortyapi.com/api/character/")
+        .then((response) => response.json())
+        .then((data) => setCharacters(data.results));
+    }, 1000);
   }, []);
+
+  const handleClick = () => {
+    setChange(!change);
+  };
 
   return (
     <div>
+      <button type="button" onClick={handleClick}>
+        {change ? "True" : "False"}
+      </button>
       {characters.map((character) => (
         <h2>{character.name}</h2>
       ))}
